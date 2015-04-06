9122d41fee0ad9240b070c82d944a16e52cb74ec
mathwhiz1212 <mathwhiz1212@gmail.com>
2015-04-05 20:16:49 -0600

    Added 32bit suppor.t

--- GnuPG1.4.19offline.sh
+++ GnuPG1.4.19offline.sh
@@ -5,10 +5,16 @@
 
 #This script installs GnuPG-1.4.19 or upgrades current version to GnuPG-1.4.19.
 
+#Asks user if they have installed gpg.
+read -r -p "Do you have a 32 bit or 64 bit operating system? [32/64] " response
+case $response in
+     32*) 
+
 #Installs necessary software.
 cd debs
+cd 32
 sudo dpkg -i *.deb
-cd .. 
+cd ../..
 
 #Extracts and compiles GnuPG.
 tar -xvjf gnupg-1.4.19.tar.bz2
@@ -25,6 +31,28 @@ read -p "Please make sure the installed version of GnuPG displayed at the top of
 #Cleans up.
 cd ..
 rm -rf gnupg-1.4.19
-   
+ ;;
+ *)
+   #Installs necessary software.
+   cd debs
+   cd 64
+   sudo dpkg -i *.deb
+   cd ../.. 
 
-   
+   #Extracts and compiles GnuPG.
+   tar -xvjf gnupg-1.4.19.tar.bz2
+   cd gnupg-1.4.19
+   ./configure --prefix=/usr
+   make 
+   sudo make install 
+
+   #Prints version of GnuPG and asks user if it is GnuPG 1.4.19.
+   clear
+   gpg --version
+   read -p "Please make sure the installed version of GnuPG displayed at the top of the terminal window is GnuPG 1.4.19 then hit enter." response
+
+   #Cleans up.
+   cd ..
+   rm -rf gnupg-1.4.19
+;;
+esac
